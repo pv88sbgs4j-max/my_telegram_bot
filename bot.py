ace91e1d3381473cc4d14f54a2267460833d2c22
Mikhail <pv88sbgs4j@privaterelay.appleid.com>
2026-08-29 13:56:59 +0300

    "First change"

--- bot.py
+++ bot.py
@@ -1,10 +1,10 @@
-# bot.py
 from telebot import TeleBot
 from config import TOKEN
 from handlers import register_handlers
 
 bot = TeleBot(TOKEN)
 register_handlers(bot)
+print("test")
 
 print("🤖 Бот запущен...")
 bot.infinity_polling()

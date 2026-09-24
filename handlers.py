7310b6cfe246910d515c58577ab154bb3d942b96
Mikhail <pv88sbgs4j@privaterelay.appleid.com>
2026-09-25 02:18:14 +0300

    "Add AI button"

--- handlers.py
+++ handlers.py
@@ -273,6 +273,7 @@ def register_handlers(bot: TeleBot):
                     if days_until > 1:
                         bot.send_message(chat_id,"Информации по составу для этого матча пока нет.\n" "Попробуйте в день матча — тогда появятся составы команд.")
                         bot.send_message(chat_id, "⬅️ Нажмите 'К матчам', чтобы вернуться", reply_markup=back_to_matches())
+                        bot.send_message(chat_id, reply_markup=ai_keyboard_for_not_stated(match_id))
                         return
                 logger.info(f"МАТЧ {match_id}: ИДЁМ В API (cached={cached is not None}, cached_score={cached_score is not None})")
                 home_data, away_data, score_data = get_match_details(match_id)
